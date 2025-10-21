660c61e1c41b6bf7e27cf29ea241e4dc5c8c27a0
Felipe Adeildo <contato@felipeadeildo.com>
2025-10-21 10:18:53 -0300

    chore: message pattern

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -12,11 +12,11 @@ def register(data: dict) -> api_response:
 
     # Validando informações faltantes
     if not all([email, password]):
-        return jsonify({"msg": "Campos obrigatórios: email, password"}), 400
+        return jsonify({"message": "Campos obrigatórios: email, password"}), 400
 
     # Validando email
     if not isinstance(email, str):
-        return jsonify({"msg": "Email inválido"}), 400
+        return jsonify({"message": "Email inválido"}), 400
 
     email_lower = email.lower()
     if not (
@@ -24,7 +24,7 @@ def register(data: dict) -> api_response:
         or email_lower.endswith("@insper.edu.br")
     ):
         return jsonify(
-            {"msg": "Utilize seu email Insper (@al.insper.edu.br ou @insper.edu.br)"}
+            {"message": "Utilize seu email Insper (@al.insper.edu.br ou @insper.edu.br)"}
         ), 400
 
     # Extraindo username do email (parte antes do @)
@@ -32,12 +32,12 @@ def register(data: dict) -> api_response:
 
     # Validando se usuario já existe
     if User.objects(email=email):
-        return jsonify({"msg": "Usuario ja existe"}), 400
+        return jsonify({"message": "Usuario ja existe"}), 400
 
     hashed = bcrypt.generate_password_hash(password).decode("utf-8")
     new_user = User(username=username, password=hashed, email=email)
     new_user.save()
-    return jsonify({"msg": "Usuario cadastrado com sucesso!"}), 201
+    return jsonify({"message": "Usuario cadastrado com sucesso!"}), 201
 
 
 def login(data: dict) -> api_response:
@@ -46,12 +46,15 @@ def login(data: dict) -> api_response:
 
     # Validando informações faltantes
     if not all([email, password]):
-        return jsonify({"msg": "Campos obrigatórios: email, password"}), 401
+        return jsonify({"message": "Campos obrigatórios: email, password"}), 401
 
     # Validando usuario e senha
     user = User.objects(email=email).first()
     if not user or not bcrypt.check_password_hash(user.password, password):
-        return jsonify({"msg": "Email ou senha inválidos"}), 401
+        return jsonify({"message": "Email ou senha inválidos"}), 401
 
     token = create_access_token(identity=user.username)
-    return jsonify(access_token=token), 200
+    return jsonify({
+        "access_token": token,
+        "user": user.to_dict()
+    }), 200
