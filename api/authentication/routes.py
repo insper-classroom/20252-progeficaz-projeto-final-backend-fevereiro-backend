3531597571210bea95e5df685933bbd798781641
Robson <robsonsf@al.insper.edu.br>
2025-10-21 06:51:23 -0400

    fix: clean up whitespace in authentication routes

--- api/authentication/routes.py
+++ api/authentication/routes.py
@@ -5,20 +5,13 @@ from flask_bcrypt import Bcrypt
 
 auth_bp = Blueprint('authentication', __name__)
 
-
-
-
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json() or {}
-
     return vi.register(data)
-   
-
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-  
     data = request.get_json() or {}
     return vi.login(data)
 
